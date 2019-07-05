933ecf286f8c92f4ec3750c8cf2ba35ae3d22021
SheekhaJ <jariwals@oregonstate.edu>
2019-07-04 18:27:19 -0700

    Basic files for Apache Jena sample project

---
+++ src/main/java/com/semanticweb/apachejenaproject/App.java
@@ -0,0 +1,50 @@
+package com.semanticweb.apachejenaproject;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.vocabulary.VCARD4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        final String personURI = "http://somewhere/personURI";
+        final String firstName = "John";
+        final String lastName = "Smith";
+        final String fullName = firstName + " " + lastName;
+        
+        Model model = ModelFactory.createDefaultModel();
+        Resource user1 = model.createResource(personURI).addProperty(VCARD4.fn, fullName);
+        Resource user2 = user1.addProperty(VCARD4.given_name, "Jane").addProperty(VCARD4.family_name, "Doe");
+        
+        StmtIterator iter = model.listStatements();
+        
+//        while(iter.hasNext()) {
+//        	Statement st = iter.nextStatement();
+//        	Resource subject = st.getSubject();
+//        	Resource predicate = st.getPredicate();
+//        	RDFNode object = st.getObject();
+//        	
+//        	System.out.println("Subject is '"+subject+"' Predicate is '"+predicate+"' Object is '"+object+"'");
+//        	
+//        	if(object instanceof Resource) {
+//        		System.out.println("Object is of type Resource - "+object.toString());
+//        	} else {
+//        		System.out.println("Object is of type Literal - "+object.toString());
+//        	}        	
+//        }
+        
+        model.write(System.out);
+    }
+}
