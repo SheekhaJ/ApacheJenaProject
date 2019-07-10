485552050299b3823aeafe5e5df80ecc075012f8
SheekhaJ <jariwals@oregonstate.edu>
2019-07-10 15:58:53 -0700

    Adding properties to modelled users

--- src/main/java/com/semanticweb/apachejenaproject/App.java
+++ src/main/java/com/semanticweb/apachejenaproject/App.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
@@ -24,21 +25,44 @@ public class App
     static final String triplesFileName = "triplesRDFOutput.rdf";
     
     static final String personURI = "http://somewhere/personURI";
+    static final String subjectURI = "http://somewhere/subjectURI";
+    
     static final String firstName = "John";
     static final String lastName = "Smith";
     static final String fullName = firstName + " " + lastName;
+    static Model model = ModelFactory.createDefaultModel();
     
-    public static void main( String[] args ) throws IOException
-    {   
-        Model model = ModelFactory.createDefaultModel();
+    public static void main( String[] args ) throws IOException{   
         Resource user1 = model.createResource(personURI).addProperty(VCARD4.fn, fullName);
-        Resource user2 = user1.addProperty(VCARD4.given_name, "Jane").addProperty(VCARD4.family_name, "Doe");
+        model.createResource(personURI).addProperty(VCARD4.fn, "Jane Doe");
+        model.createResource(personURI).addProperty(VCARD4.fn, "Riley Smith");
+        
+        Resource multithreadingResource = model.createResource(subjectURI).addProperty(VCARD4.category, "language").addProperty(VCARD4.category, "feature").addLiteral(VCARD4.value, "multithreading");
+        Resource pythonResource =  model.createResource(subjectURI).addProperty(VCARD4.category, "language").addLiteral(VCARD4.value, "python");
+        Resource javascriptResource = model.createResource(subjectURI).addProperty(VCARD4.category, "language").addLiteral(VCARD4.value, "javascript");
+        Resource axiosResource = model.createResource(subjectURI).addProperty(VCARD4.value, "axios");
+        
+//        user1.addProperty(VCARD4.value, multithreadingResource);
+        user1.addProperty(VCARD4.value, pythonResource);
+        user1.addProperty(VCARD4.value, javascriptResource);
+        
+//        user1.add
+        
+//        Resource user2 = user1.addProperty(VCARD4.given_name, "Jane").addProperty(VCARD4.family_name, "Doe");
         
         readStatements(model);
         
         writeRDF(model);
         
         readRDF();
+    }
+    
+    private static Resource createResource(String uri) {
+    	return model.createResource(uri);
+    }
+    
+    private static Resource createResource(String uri, Property property, String propertyString) {
+    	return model.createResource(uri).addProperty(property, propertyString);
     }
     
     private static void readStatements(Model model) {
@@ -52,11 +76,11 @@ public class App
       	
       	System.out.println("Subject is '"+subject+"' Predicate is '"+predicate+"' Object is '"+object+"'");
       	
-      	if(object instanceof Resource) {
-      		System.out.println("Object is of type Resource - "+object.toString());
-      	} else {
-      		System.out.println("Object is of type Literal - "+object.toString());
-      	}        	
+//      	if(object instanceof Resource) {
+//      		System.out.println("Object is of type Resource - "+object.toString());
+//      	} else {
+//      		System.out.println("Object is of type Literal - "+object.toString());
+//      	}        	
       }
     }
     
